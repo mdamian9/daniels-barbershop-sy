a909b6991940dd2fe5b7c748c03378a12bf27021
mdamian9 <m.damian9@hotmail.com>
2018-08-30 01:39:00 -0700

    Created public folder + some initial components

---
+++ client/src/components/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import "./HomePage.css";
+
+class HomePage extends Component {
+    render = () => {
+        return (
+            <div className="og-logo"><img src="/assets/images/logo_og.jpeg" alt="original-logo"/></div>
+        );
+    };
+};
+
+export default HomePage;
