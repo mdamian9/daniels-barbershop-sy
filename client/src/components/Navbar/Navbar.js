4e37fec5bb473a946a083cb011a1651f9e215f77
mdamian9 <m.damian9@hotmail.com>
2018-09-11 17:14:28 -0700

    Added CSS to drawer toggle button

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./Navbar.css"
-import "../SideDrawer/DrawerToggleButton";
 import DrawerToggleButton from "../SideDrawer/DrawerToggleButton";
 
 const navbar = (props) => {
