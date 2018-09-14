300bc11db2dd06138758c50d19b51c66bc0bf9a2
mdamian9 <m.damian9@hotmail.com>
2018-09-13 23:58:12 -0700

    Updated Footer component CSS

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css"
 const footer = (props) => {
     return (
         <div className="footer-component">
-            <div className="footer-content">
+            <div className="footer-content text-center">
                 (619) 371-8571&ensp;<i class="fas fa-circle dot"></i>&ensp;
                 <a href="https://www.google.com/maps/place/Daniel's+Barbershop/@32.5534624,-117.0466422,15z/data=!4m5!3m4!1s0x0:0x7db518c49d92580b!8m2!3d32.5534624!4d-117.0466422" target="_blank">
                     161 W San Ysidro Blvd, Suite C, San Diego, CA 92173
