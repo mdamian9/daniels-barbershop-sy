7f30bdf1b937478ad269ce8ca42738ad135d5bfa
mdamian9 <m.damian9@hotmail.com>
2018-09-15 03:10:02 -0700

    Changed media queries for Footer / updated CSS

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -5,14 +5,14 @@ const footer = (props) => {
     return (
         <div className="footer-component">
             <div className="footer-content text-center">
-                (619) 371-8571&ensp;<i class="fas fa-circle dot"></i>&ensp;
+                (619) 371-8571&ensp;&ensp;<i class="fas fa-circle dot"></i>&ensp;&ensp;
                 <a href="https://www.google.com/maps/place/Daniel's+Barbershop/@32.5534624,-117.0466422,15z/data=!4m5!3m4!1s0x0:0x7db518c49d92580b!8m2!3d32.5534624!4d-117.0466422" target="_blank">
                     161 W San Ysidro Blvd, Suite C, San Diego, CA 92173
                 </a>
-                &ensp;<i class="fas fa-circle dot"></i>&ensp;&nbsp;
+                &ensp;&ensp;<i class="fas fa-circle dot"></i>&ensp;&ensp;
                 <a href="https://www.instagram.com/danielsbarbershop_/" target="_blank">
                     <i className="fab fa-instagram"></i>
-                </a>&ensp;
+                </a>&ensp;&ensp;
                 <a href="https://www.yelp.com/biz/daniels-barbershop-san-diego" target="_blank">
                     <i className="fab fa-yelp"></i>
                 </a>
