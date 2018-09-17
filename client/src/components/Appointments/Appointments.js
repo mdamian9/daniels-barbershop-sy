d9ed811595eb50eba734eb0adf779256a2fb92eb
mdamian9 <m.damian9@hotmail.com>
2018-09-16 23:28:47 -0700

    Deleted unnecessary code

--- client/src/components/Appointments/Appointments.js
+++ client/src/components/Appointments/Appointments.js
@@ -54,43 +54,6 @@ class Appointments extends Component {
                         <h1><i>Appointments</i></h1>
                     </header>
                     <AppointmentForm />
-                    {/* <div className="card appt-form">
-                        <form>
-                            <div className="form-group">
-                                <label htmlFor="firstName"><b>First name:</b></label>
-                                <input type="text" className="form-control" id="firstName" placeholder="Enter your first name"></input>
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="lastName"><b>Last name:</b></label>
-                                <input type="text" className="form-control" id="lastName" placeholder="Enter your last name"></input>
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="firstName"><b>Phone number:</b></label>
-                                <input type="text" className="form-control" id="firstName" placeholder="1234567890"></input>
-                                <small>
-                                    <i>Enter 10 digit phone number (numbers only)</i>
-                                </small>
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="userEmail"><b>Email:</b></label>
-                                <input type="emai" className="form-control" id="userEmail" placeholder="user@email.com"></input>
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="serviceDescription"><b>Haircut / service description:</b></label>
-                                <textarea className="form-control" id="serviceDescription" rows="3"></textarea>
-                            </div>
-                            <div className="form-group">
-                                <b>Choose a date: </b>
-                                <DatePicker selected={this.state.date}
-                                    onSelect={this.handleSelect} // when day is clicked
-                                    onChange={this.handleChange} // only when value has changed
-                                />
-                            </div>
-                            <div className="form-group text-center">
-                                <button type="submit" className="btn btn-default">Submit</button>
-                            </div>
-                        </form>
-                    </div> */}
                     <div className="text-center">
                         <p><b>Site under construction.</b></p>
                     </div>
