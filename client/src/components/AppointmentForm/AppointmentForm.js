49bf10962aebca7129dc3c42129cb5bd5b2b4f00
mdamian9 <m.damian9@hotmail.com>
2018-09-17 17:11:15 -0700

    Fixed state of AppointmentForm component

--- client/src/components/AppointmentForm/AppointmentForm.js
+++ client/src/components/AppointmentForm/AppointmentForm.js
@@ -9,11 +9,11 @@ class AppointmentForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            firstName: ''
-            // lastName: '',
-            // phoneNumber: '',
-            // userEmail: '',
-            // description: ''
+            firstName: '',
+            lastName: '',
+            phoneNumber: '',
+            userEmail: '',
+            serviceDescription: ''
         };
 
         this.handleChange = this.handleChange.bind(this);
@@ -22,16 +22,12 @@ class AppointmentForm extends Component {
 
     handleChange(event) {
         this.setState({
-            firstName: event.target.firstName
-            // lastName: event.target.lastName,
-            // phoneNumber: event.target.phoneNumber,
-            // userEmail: event.target.userEmail,
-            // description: event.target.description
+            [event.target.name]: event.target.value
         });
     };
 
     handleSubmit(event) {
-        alert('First name:' + this.state.firstName);
+        alert('First name:' + this.state.value);
         event.preventDefault();
     };
 
@@ -40,27 +36,27 @@ class AppointmentForm extends Component {
             <div className="card appt-form">
                 <form onSubmit={this.handleSubmit}>
                     <div className="form-group">
-                        <label htmlFor="firstName"><b>First name:</b></label>
-                        <input type="text" className="form-control" id="firstName" placeholder="Enter your first name" value={this.state.firstName} onChange={this.handleChange} />
+                        <label htmlFor="value"><b>First name:</b></label>
+                        <input type="text" className="form-control" name="firstName" placeholder="Enter your first name" value={this.state.value} onChange={this.handleChange} />
                     </div>
                     <div className="form-group">
                         <label htmlFor="lastName"><b>Last name:</b></label>
-                        <input type="text" className="form-control" id="lastName" placeholder="Enter your last name" value={this.state.lastName} onChange={this.handleChange} />
+                        <input type="text" className="form-control" name="lastName" placeholder="Enter your last name" value={this.state.value} onChange={this.handleChange} />
                     </div>
                     <div className="form-group">
                         <label htmlFor="phoneNumber"><b>Phone number:</b></label>
-                        <input type="text" className="form-control" id="firstName" placeholder="1234567890" value={this.state.phoneNumber} onChange={this.handleChange} />
+                        <input type="text" className="form-control" name="phoneNumber" placeholder="1234567890" value={this.state.value} onChange={this.handleChange} />
                         <small>
                             <i>Enter 10 digit phone number (numbers only)</i>
                         </small>
                     </div>
                     <div className="form-group">
                         <label htmlFor="userEmail"><b>Email:</b></label>
-                        <input type="emai" className="form-control" id="userEmail" placeholder="user@email.com" value={this.state.userEmail} onChange={this.handleChange} />
+                        <input type="emai" className="form-control" name="userEmail" placeholder="user@email.com" value={this.state.value} onChange={this.handleChange} />
                     </div>
                     <div className="form-group">
                         <label htmlFor="serviceDescription"><b>Haircut / service description:</b></label>
-                        <textarea className="form-control" id="serviceDescription" rows="3" value={this.state.description} onChange={this.handleChange} />
+                        <textarea className="form-control" name="serviceDescription" rows="3" value={this.state.value} onChange={this.handleChange} />
                     </div>
                     <div className="form-group">
                         <b>Choose a date: </b>
